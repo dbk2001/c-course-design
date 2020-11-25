91da56c0122192cfd4c58ca730a0732bddeb4472
dbk2001 <69680866+dbk2001@users.noreply.github.com>
2020-11-25 20:03:32 +0800

    Update AIchess.h

--- AIchess.h
+++ AIchess.h
@@ -2,15 +2,17 @@
 #define _AIchess_H_
 
 #include"GoBang.h"
+
+
 //point结构体，用来表示点的坐标
 struct point{
     int col;
     int row;
 }; 
 
+
 //drctn结构体，用来表示向量
 //其可以将4个方向用一个简单的drctn d的方向变量来处理
-
 struct drctn{
     int dy;
     int dx;
@@ -29,18 +31,24 @@ private:
     int m_computer;//电脑棋子 
     point m_luozi;//落子位置 ，包含横纵坐标
 public:
-/**********************
-参数：int i
-返回值：void
-功能：人机对战的中心操作
-**********************/
+/***************************
+构造函数
+析构函数
+****************************/
 	operate();
 	~operate() {};
-    void centrOprt(int i, GoBang &g);
+	
+	
+/*************************
+参数：int i， GoBang &g
+返回值：void
+功能：人机对战的中心操作
+*************************/
+    void centrOprt(int n, GoBang &g);
 	
 
 /************************************
-参数：无
+参数：GoBang &g
 返回值：void
 功能：电脑操作部分，调用calScore()函数
 统计每个空位置的最大分值，决定进攻或防守
@@ -50,7 +58,7 @@ public:
 
 
 /**************************************
-参数：无
+参数：GoBang &g
 返回值：1或0
 功能：判断游戏是否结束，即是否连成五子
 **************************************/
@@ -67,7 +75,7 @@ protected:
 
 
 /**********************************
-参数：point p,int side
+参数：point p,int side，GoBang &g
 返回值：int(最高得分)
 功能：计算电脑下一步走法的得分，分为
 进攻得分和防守得分
@@ -81,7 +89,11 @@ protected:
 返回值：point结构体，实际上就是返回一个向量
 功能：计算出并返回新的向量
 ***********************************************************/
-    point newPoint(point p, drctn d, int lenth);
+    point newPoint(point p, drctn d, int lenth)
+	{
+    point p1 = {p.col + d.dy * lenth, p.row + d.dx * lenth};
+    return p1;
+    }
 };
 
 
